7901de5b5ca26ccae48b9d176e998c519bc71f75
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2022-04-30 23:01:26 +0200

    initial

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { spawn } = require('child_process');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+  const myToken = core.getInput('repo-token');
+
+  const octokit = github.getOctokit(myToken)
+
+  const ls = spawn('git', ['--version']);
+
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  return
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner: 'octokit',
+    repo: 'rest.js',
+    pull_number: 123,
+    mediaType: {
+      format: 'diff'
+    }
+  });
+
+  console.log(pullRequest);
+
 } catch (error) {
   core.setFailed(error.message);
 }
