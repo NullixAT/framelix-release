267be871ffaa82c5d89ac764169d13969d99177e
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2022-05-01 20:20:19 +0200

    initial

--- index.js
+++ index.js
@@ -116,6 +116,8 @@ function deleteRecursive (folder) {
       core.info('===Uploading docker-release.zip asset===')
       zip = new AdmZip()
       zip.addLocalFolder(cwd + '/export')
+      zip.addLocalFile(__dirname + '/src/install.php', 'install.php')
+      zip.addLocalFile(__dirname + '/src/check-requirements.php', 'check-requirements.php')
       await octokit.rest.repos.uploadReleaseAsset({
         owner: repoSplit[0],
         repo: repoSplit[1],
@@ -126,9 +128,9 @@ function deleteRecursive (folder) {
       core.info('✓ Done')
       core.info('')
 
-      core.info('')
-      core.info('')
-      core.info('✓✓✓ All done')
+      core.info('✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓')
+      core.info('✓✓✓ All done ✓✓✓')
+      core.info('✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓')
     }
   } catch (error) {
     core.setFailed(error.message)
